2f5ec6254c0dd2d3f7281cbaa34b1f287f09a586
高倉楓麻 <ai24cuz@gmail.com>
2019-11-15 15:29:20 +0800

    Initial commit

---
+++ UserDefaultProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultProject
+//
+//  Created by 高倉楓麻 on 2019/11/15.
+//  Copyright © 2019 高倉楓麻. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
